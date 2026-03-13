30a506986d42e59a158497663e1f0a3e7e50645f
manhtv <manhtv@zinza.com.vn>
2026-03-13 13:47:25 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,25 @@ import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [ tanstackRouter({ autoCodeSplitting: true }), react(), tailwindcss()],
+  base: '/',
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@lib': path.resolve(__dirname, './src/lib'),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // allowedHosts: ['cs-chatbot-test.fc2.com', 'localhost'],
   },
 })
